68fc7a90375e5620361d6ffd7c8a798b18af1e97
moosamahroof <mahroofanchukandan@gmail.com>
2022-12-21 12:23:39 +0530

    Initial commit

---
+++ lib/data/dummyData.dart
@@ -0,0 +1,41 @@
+
+
+List<Map> VideoData = [
+  {
+    'thumb': 'https://i.ytimg.com/vi/DVGYddFaLv0/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC4CBLyPfp5nKTgIuFVqMFqAiVxwQ',
+    'duration': '2:44',
+    'title': 'NavigationBar (Widget of the Week)',
+    'profile': 'https://yt3.ggpht.com/ytc/AMLnZu9gYALpdyk32YZAlIP5r7MxKBcknAS7FnM9v32E=s88-c-k-c0x00ffffff-no-rj',
+    'chname': 'Flutter',
+    'views': 45,
+    'date': '20 minutes ago',
+  },
+  {
+    'thumb': 'https://topicimages.mrowl.com/large/techtraveleat/techtraveleat_2.jpg',
+    'duration': '11:18',
+    'title': 'nepal vlog 3 |( 4K Ultra Graphics )',
+    'profile': 'https://yt3.ggpht.com/ytc/AMLnZu-5eDWAa8PE11D2FApCz6qGdCROvjCMAJD-Tl9iZA=s88-c-k-c0x00ffffff-no-rj',
+    'chname': 'tech travel eat',
+    'views': 10,
+    'date': 'just now',
+  },
+
+  {
+    'thumb': 'https://cdn.mobilesyrup.com/wp-content/uploads/2022/09/iphone-14-pro-header-1-scaled.jpg',
+    'duration': '9:22',
+    'title': 'iPhone 14 Pro max user review',
+    'profile': 'https://fscl01.fonpit.de/userfiles/7687254/image/Apple_iPhone_14_Pro_Max/NextPit_Apple_iPhone_14_Pro_Max_Review-w1400h788.jpg',
+    'chname': 'mr: techy',
+    'views': '7.1 lakh',
+    'date': '1 month ago',
+  },
+  {
+    'thumb': 'https://cdn.mobilesyrup.com/wp-content/uploads/2022/09/iphone-14-pro-header-1-scaled.jpg',
+    'duration': '9:22',
+    'title': 'iPhone 14 Pro max user review',
+    'profile': 'https://fscl01.fonpit.de/userfiles/7687254/image/Apple_iPhone_14_Pro_Max/NextPit_Apple_iPhone_14_Pro_Max_Review-w1400h788.jpg',
+    'chname': 'mr: techy',
+    'views': '7.1 lakh',
+    'date': '1 month ago',
+  },
+];
